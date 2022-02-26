3be7e9d119aa85ef4d52713397a2548d6eb731f9
htlsne <htlsne23@gmail.com>
2022-02-26 19:24:23 +0900

    Connect to database

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,34 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use sqlx::{postgres::PgPoolOptions, PgPool};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello))
-        .bind(("0.0.0.0", fetch_port()))?
-        .run()
+    let db_url = fetch_database_url();
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&db_url)
         .await
+        .unwrap();
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(pool.clone()))
+            .service(hello)
+    })
+    .bind(("0.0.0.0", fetch_port()))?
+    .run()
+    .await
 }
 
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn hello(pool: web::Data<PgPool>) -> impl Responder {
+    let row: (i64,) = sqlx::query_as("SELECT $1")
+        .bind(150_i64)
+        .fetch_one(pool.get_ref())
+        .await
+        .unwrap();
+    HttpResponse::Ok().body(row.0.to_string())
 }
 
 fn fetch_port() -> u16 {
@@ -22,5 +40,15 @@ fn fetch_port() -> u16 {
             .expect("Failed to parse environment variable PORT."),
         Err(VarError::NotPresent) => panic!("Environment variable PORT is required."),
         Err(VarError::NotUnicode(_)) => panic!("Environment variable PORT is not unicode."),
+    }
+}
+
+fn fetch_database_url() -> String {
+    use std::env::VarError;
+
+    match std::env::var("DATABASE_URL") {
+        Ok(s) => s,
+        Err(VarError::NotPresent) => panic!("Environment variable DATABASE_URL is required."),
+        Err(VarError::NotUnicode(_)) => panic!("Environment variable DATABASE_URL is not unicode."),
     }
 }
