81dfaf66b4aa03d4c00f26905f0d99bb5e10197f
htlsne <htlsne23@gmail.com>
2022-02-26 12:51:45 +0900

    Fetch port from env var

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
-use actix_web::{HttpServer, App, get, Responder, HttpResponse};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-    })
-    .bind("0.0.0.0:8080")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(hello))
+        .bind(("0.0.0.0", fetch_port()))?
+        .run()
+        .await
 }
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
+}
+
+fn fetch_port() -> u16 {
+    std::env::var("PORT")
+        // TODO: エラー処理を細かく
+        .unwrap_or("8080".to_string())
+        .parse()
+        .expect("Failed to parse environment variable PORT.")
 }
