9eb502eea6bc0085b741b7f795247cdd96bdb71b
hiterm <hiterm23@gmail.com>
2022-03-12 21:07:38 +0900

    Implement UserRepository

--- src/infrastructure/user_repository.rs
+++ src/infrastructure/user_repository.rs
@@ -1,4 +1,4 @@
-use sqlx::{PgPool, PgConnection};
+use sqlx::{PgConnection, PgPool};
 use uuid::Uuid;
 
 #[derive(sqlx::FromRow)]
@@ -12,6 +12,15 @@ struct UserRepository {
 }
 
 impl UserRepository {
+    async fn find_by_id(&self, id: Uuid) -> User {
+        let mut conn = self.pool.acquire().await.unwrap();
+        InternalUserRepository::find_by_id(id, &mut conn).await
+    }
+}
+
+struct InternalUserRepository {}
+
+impl InternalUserRepository {
     async fn find_by_id(id: Uuid, conn: &mut PgConnection) -> User {
         let user: User = sqlx::query_as("SELECT * FROM bookshelf_user WHERE id = ?")
             .bind(id)
