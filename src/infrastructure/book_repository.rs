44cb493142ffce53a5c9da090495e3e460db4b79
hiterm <hiterm23@gmail.com>
2022-03-12 19:39:14 +0900

    Implement find_all_book_rows

--- src/infrastructure/book_repository.rs
+++ src/infrastructure/book_repository.rs
@@ -1,78 +1,29 @@
-use sqlx::{
-    database::HasArguments, Acquire, ColumnIndex, Database, Executor, IntoArguments, PgConnection,
-    PgExecutor, Pool, Postgres, Type,
-};
+use sqlx::{PgConnection, PgPool};
 use time::PrimitiveDateTime;
 use uuid::Uuid;
 
 use crate::domain::book::Book;
 
-struct BookRepository<E> {
-    executor: E,
+struct BookRepository {
+    pool: PgPool,
 }
 
-impl<'a, E> BookRepository<E>
-where
-    E: Executor<'a>,
-    <E::Database as HasArguments<'a>>::Arguments: IntoArguments<'a, <E as Executor<'a>>::Database>,
-{
-    // async fn findAll(&self) -> Vec<Book> {
-    //     let book_rows: Vec<BookRow> = sqlx::query_as("SELECT 1")
-    //         .fetch_all(self.executor)
-    //         .await
-    //         .unwrap();
-
-    //     todo!()
-    // }
-
-    // async fn find_by_id(&self) -> Book {
-    //     self.executor.fetch_one("SELECT 1").await;
-    //     // let book_rows = sqlx::query("SELECT 1")
-    //     //     .fetch_one(self.executor)
-    //     //     .await
-    //     //     .unwrap();
-
-    //     todo!()
-    // }
+impl BookRepository {
+    async fn find_all_book_rows(conn: &mut PgConnection) -> Vec<BookRow> {
+        let book_rows: Vec<BookRow> = sqlx::query_as("SELECT * FROM book")
+            .fetch_all(conn)
+            .await
+            .unwrap();
+        book_rows
+    }
 }
-
-// async fn find_by_id<'a, E>(executor: E) -> Book
-// where
-//     E: Executor<'a>,
-//     <E::Database as HasArguments<'a>>::Arguments: IntoArguments<'a, <E as Executor<'a>>::Database>,
-// {
-//     let book_rows = sqlx::query("SELECT 1").fetch_one(executor).await.unwrap();
-//     todo!()
-// }
-
-// async fn find_by_id<DB>(pool: &Pool<DB>)
-// where
-//     DB: Database,
-//     <DB as HasArguments<'_>>::Arguments: IntoArguments<'_, DB>,
-//     &'c mut <DB as sqlx::Database>::Connection: for<'c> Executor<'c>,
-// {
-//     sqlx::query("SELECT 1").fetch_one(pool).await.unwrap();
-// }
-
-// async fn find_by_id<'a, A>(conn: A) -> Book
-// where
-//     A: Acquire<'a, Database = Postgres> + Send + 'a,
-// {
-//     let mut conn = conn.acquire().await.unwrap();
-
-//     let book_row: (i32,) = sqlx::query_as("SELECT 1")
-//         .fetch_one(&mut *conn)
-//         .await
-//         .unwrap();
-//     todo!()
-// }
 
 async fn query_1(conn: &mut PgConnection) -> i32 {
     let book_row: (i32,) = sqlx::query_as("SELECT 1").fetch_one(conn).await.unwrap();
     book_row.0
 }
 
-// #[derive(sqlx::FromRow)]
+#[derive(sqlx::FromRow)]
 struct BookRow {
     id: Uuid,
     user_id: String,
@@ -94,7 +45,7 @@ mod tests {
     use sqlx::postgres::PgPoolOptions;
 
     #[tokio::test]
-    async fn connect() {
+    async fn test_query_1() {
         dotenv::dotenv().ok();
 
         let db_url = fetch_database_url();
@@ -111,6 +62,26 @@ mod tests {
         tx.rollback().await.unwrap();
 
         assert_eq!(actual, 1);
+    }
+
+    #[tokio::test]
+    async fn test_find_all() {
+        dotenv::dotenv().ok();
+
+        let db_url = fetch_database_url();
+        let pool = PgPoolOptions::new()
+            .max_connections(5)
+            .connect_timeout(Duration::from_secs(1))
+            .connect(&db_url)
+            .await
+            .unwrap();
+        let mut tx = pool.begin().await.unwrap();
+
+        let actual = BookRepository::find_all_book_rows(&mut tx).await;
+
+        tx.rollback().await.unwrap();
+
+        assert_eq!(actual.len(), 0);
     }
 
     fn fetch_database_url() -> String {
