f1eaec43d04e8b32fc272f96d1a36bec01b83cd8
hiterm <hiterm23@gmail.com>
2022-03-12 19:13:21 +0900

    Test using PostgreSQL

--- src/infrastructure/book_repository.rs
+++ src/infrastructure/book_repository.rs
@@ -1,5 +1,9 @@
-use sqlx::Executor;
+use sqlx::{
+    database::HasArguments, Acquire, ColumnIndex, Database, Executor, IntoArguments, PgConnection,
+    PgExecutor, Pool, Postgres, Type,
+};
 use time::PrimitiveDateTime;
+use uuid::Uuid;
 
 use crate::domain::book::Book;
 
@@ -10,26 +14,72 @@ struct BookRepository<E> {
 impl<'a, E> BookRepository<E>
 where
     E: Executor<'a>,
+    <E::Database as HasArguments<'a>>::Arguments: IntoArguments<'a, <E as Executor<'a>>::Database>,
 {
-    fn findAll(&self) -> Vec<Book> {
-        let book_rows: Vec<BookRow> = sqlx::query_as("SELECT $1")
-            .bind(150_i64)
-            .fetch_all(self.executor)
-            .await
-            .unwrap();
+    // async fn findAll(&self) -> Vec<Book> {
+    //     let book_rows: Vec<BookRow> = sqlx::query_as("SELECT 1")
+    //         .fetch_all(self.executor)
+    //         .await
+    //         .unwrap();
 
-        todo!()
-    }
+    //     todo!()
+    // }
+
+    // async fn find_by_id(&self) -> Book {
+    //     self.executor.fetch_one("SELECT 1").await;
+    //     // let book_rows = sqlx::query("SELECT 1")
+    //     //     .fetch_one(self.executor)
+    //     //     .await
+    //     //     .unwrap();
+
+    //     todo!()
+    // }
 }
 
-#[derive(sqlx::FromRow)]
+// async fn find_by_id<'a, E>(executor: E) -> Book
+// where
+//     E: Executor<'a>,
+//     <E::Database as HasArguments<'a>>::Arguments: IntoArguments<'a, <E as Executor<'a>>::Database>,
+// {
+//     let book_rows = sqlx::query("SELECT 1").fetch_one(executor).await.unwrap();
+//     todo!()
+// }
+
+// async fn find_by_id<DB>(pool: &Pool<DB>)
+// where
+//     DB: Database,
+//     <DB as HasArguments<'_>>::Arguments: IntoArguments<'_, DB>,
+//     &'c mut <DB as sqlx::Database>::Connection: for<'c> Executor<'c>,
+// {
+//     sqlx::query("SELECT 1").fetch_one(pool).await.unwrap();
+// }
+
+// async fn find_by_id<'a, A>(conn: A) -> Book
+// where
+//     A: Acquire<'a, Database = Postgres> + Send + 'a,
+// {
+//     let mut conn = conn.acquire().await.unwrap();
+
+//     let book_row: (i32,) = sqlx::query_as("SELECT 1")
+//         .fetch_one(&mut *conn)
+//         .await
+//         .unwrap();
+//     todo!()
+// }
+
+async fn query_1(conn: &mut PgConnection) -> i32 {
+    let book_row: (i32,) = sqlx::query_as("SELECT 1").fetch_one(conn).await.unwrap();
+    book_row.0
+}
+
+// #[derive(sqlx::FromRow)]
 struct BookRow {
-    id: String,
+    id: Uuid,
     user_id: String,
     isbn: Option<String>,
     read: bool,
     owned: bool,
-    priority: u32,
+    priority: i32,
     format: Option<String>,
     store: Option<String>,
     created_at: PrimitiveDateTime,
@@ -38,16 +88,40 @@ struct BookRow {
 
 #[cfg(test)]
 mod tests {
-    use sqlx::{Connection, SqliteConnection};
+    use std::time::Duration;
+
+    use super::*;
+    use sqlx::postgres::PgPoolOptions;
 
     #[tokio::test]
     async fn connect() {
-        let mut conn = SqliteConnection::connect("sqlite::memory:").await.unwrap();
-        let row: (i64,) = sqlx::query_as("SELECT $1")
-            .bind(150_i64)
-            .fetch_one(&mut conn)
+        dotenv::dotenv().ok();
+
+        let db_url = fetch_database_url();
+        let pool = PgPoolOptions::new()
+            .max_connections(5)
+            .connect_timeout(Duration::from_secs(1))
+            .connect(&db_url)
             .await
             .unwrap();
-        assert_eq!(row.0, 150);
+        let mut tx = pool.begin().await.unwrap();
+
+        let actual = query_1(&mut tx).await;
+
+        tx.rollback().await.unwrap();
+
+        assert_eq!(actual, 1);
+    }
+
+    fn fetch_database_url() -> String {
+        use std::env::VarError;
+
+        match std::env::var("DATABASE_URL") {
+            Ok(s) => s,
+            Err(VarError::NotPresent) => panic!("Environment variable DATABASE_URL is required."),
+            Err(VarError::NotUnicode(_)) => {
+                panic!("Environment variable DATABASE_URL is not unicode.")
+            }
+        }
     }
 }
