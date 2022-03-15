758dd0fce330f50901b97603d92c84c62a24c7bd
hiterm <hiterm23@gmail.com>
2022-03-15 22:00:14 +0900

    refactor: Apply New Type pattern for Book

--- src/domain/entity/book.rs
+++ src/domain/entity/book.rs
@@ -1,27 +1,72 @@
 // TODO: 消す
 #![allow(warnings, unused)]
 
+use getset::Getters;
 use time::PrimitiveDateTime;
+use uuid::Uuid;
+
+use crate::domain::error::domain_error::DomainError;
 
 use super::author::Author;
 
-pub struct BookshelfUser {
-    id: String,
-    sub: String,
+#[derive(Debug, Clone, PartialEq, Eq, Getters)]
+struct BookId {
+    #[getset(get = "pub")]
+    id: Uuid,
 }
 
-pub struct Book {
-    id: String,
-    user: BookshelfUser,
-    authors: Vec<Author>,
-    isbn: Option<String>,
-    read: bool,
-    owned: bool,
-    priority: u32,
-    format: Option<BookFormat>,
-    store: Option<BookStore>,
-    created_at: PrimitiveDateTime,
-    updated_at: PrimitiveDateTime,
+impl BookId {
+    pub fn new(id: Uuid) -> Result<BookId, DomainError> {
+        Ok(BookId { id })
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Getters)]
+struct Isbn {
+    #[getset(get = "pub")]
+    value: String,
+}
+
+impl Isbn {
+    pub fn new(value: String) -> Result<Isbn, DomainError> {
+        Ok(Isbn { value })
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Getters)]
+struct ReadFlag {
+    #[getset(get = "pub")]
+    value: bool,
+}
+
+impl ReadFlag {
+    pub fn new(value: bool) -> Result<ReadFlag, DomainError> {
+        Ok(ReadFlag { value })
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Getters)]
+struct OwnedFlag {
+    #[getset(get = "pub")]
+    value: bool,
+}
+
+impl OwnedFlag {
+    pub fn new(value: bool) -> Result<OwnedFlag, DomainError> {
+        Ok(OwnedFlag { value })
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Getters)]
+struct Priority {
+    #[getset(get = "pub")]
+    value: i32,
+}
+
+impl Priority {
+    pub fn new(value: i32) -> Result<Priority, DomainError> {
+        Ok(Priority { value })
+    }
 }
 
 pub enum BookFormat {
@@ -31,4 +76,17 @@ pub enum BookFormat {
 
 pub enum BookStore {
     Kindle,
+}
+
+pub struct Book {
+    id: BookId,
+    authors: Vec<Author>,
+    isbn: Option<Isbn>,
+    read: ReadFlag,
+    owned: OwnedFlag,
+    priority: Priority,
+    format: Option<BookFormat>,
+    store: Option<BookStore>,
+    created_at: PrimitiveDateTime,
+    updated_at: PrimitiveDateTime,
 }
