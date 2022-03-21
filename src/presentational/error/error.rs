8ecd8fb465785452cf82d8b19fe51bf98b9ab1b7
hiterm <hiterm23@gmail.com>
2022-03-21 16:36:48 +0900

    Remove unnecessary impl

--- src/presentational/error/error.rs
+++ src/presentational/error/error.rs
@@ -5,9 +5,3 @@ pub enum PresentationalError {
     #[error(transparent)]
     OtherError(anyhow::Error),
 }
-
-impl From<uuid::Error> for PresentationalError {
-    fn from(error: uuid::Error) -> Self {
-        PresentationalError::OtherError(anyhow::Error::new(error))
-    }
-}
