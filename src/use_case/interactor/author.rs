99d0a4c4b0143aee4f2f64abb904bc1647d687e9
hiterm <hiterm23@gmail.com>
2022-03-21 15:42:53 +0900

    Add test for ShowAuthorInteractor

--- src/use_case/interactor/author.rs
+++ src/use_case/interactor/author.rs
@@ -1,5 +1,4 @@
 use async_trait::async_trait;
-use uuid::Uuid;
 
 use crate::{
     domain::{
@@ -11,6 +10,12 @@ use crate::{
 
 struct ShowAuthorInteractor<ARepo> {
     author_repository: ARepo,
+}
+
+impl<ARepo> ShowAuthorInteractor<ARepo> {
+    pub fn new(author_repository: ARepo) -> Self {
+        ShowAuthorInteractor { author_repository }
+    }
 }
 
 #[async_trait]
@@ -35,5 +40,68 @@ where
                 user_id: raw_author_id.to_string(),
             })
             .map(|author| -> Author { author.into() })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use async_trait::async_trait;
+    use mockall::{mock, predicate::always};
+
+    use crate::{
+        domain::{
+            self,
+            entity::{
+                author::{AuthorId, AuthorName},
+                user::UserId,
+            },
+            error::DomainError,
+            repository::author_repository::AuthorRepository,
+        },
+        use_case::use_case::author::ShowAuthorUseCase,
+    };
+
+    use super::ShowAuthorInteractor;
+
+    mock! {
+        Repo {}
+        #[async_trait]
+        impl AuthorRepository for Repo {
+            async fn create(&self, user_id: &UserId, author: &domain::entity::author::Author) -> Result<(), DomainError>;
+            async fn find_by_id(
+                &self,
+                user_id: &UserId,
+                author_id: &AuthorId,
+            ) -> Result<Option<domain::entity::author::Author>, DomainError>;
+        }
+    }
+
+    #[tokio::test]
+    async fn find_by_id() {
+        let mut author_repository = MockRepo::new();
+
+        let user_id = "user1";
+        let author_id = "006099b4-6c42-4ec4-8645-f6bd5b63eddc";
+        let author_name = "author1";
+
+        author_repository
+            .expect_find_by_id()
+            .with(always(), always())
+            .returning(|_, _| {
+                Ok(Some(domain::entity::author::Author {
+                    id: AuthorId::new(author_id).unwrap(),
+                    name: AuthorName::new(author_name.to_string()).unwrap(),
+                }))
+            });
+
+        let show_author_interactor = ShowAuthorInteractor::new(author_repository);
+
+        let author = show_author_interactor
+            .find_by_id(user_id, author_id)
+            .await
+            .unwrap();
+
+        assert_eq!(author.id, author_id);
+        assert_eq!(author.name, author_name);
     }
 }
