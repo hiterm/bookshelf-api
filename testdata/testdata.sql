fd8dd8758b73d5bf56d6c25a911851f164daa66c
hiterm <hiterm23@gmail.com>
2022-05-04 18:11:19 +0900

    Update testdata

--- testdata/testdata.sql
+++ testdata/testdata.sql
@@ -6,6 +6,10 @@ INSERT INTO author
 (id, user_id, name)
 VALUES
 ('9d548ede-f636-4890-8418-ad3b7336d8e3', 'user1', 'author1');
+INSERT INTO author
+(id, user_id, name)
+VALUES
+('54bbfcd2-e937-4da5-84fc-5984fa7b5979', 'user1', 'author2');
 
 INSERT INTO book (
   id,
@@ -30,4 +34,8 @@ VALUES (
   'Kindle'
 );
 
-ROLLBACK;
+INSERT INTO book_author (book_id, author_id) VALUES
+('d85f0e56-f632-4d50-b057-bab5af3d0159', '9d548ede-f636-4890-8418-ad3b7336d8e3'),
+('d85f0e56-f632-4d50-b057-bab5af3d0159', '54bbfcd2-e937-4da5-84fc-5984fa7b5979');
+
+COMMIT;
