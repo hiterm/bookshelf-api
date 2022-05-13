f0ef5eea713fc4d53396ac43d268b5c5302cabfd
hiterm <hiterm23@gmail.com>
2022-05-13 22:23:52 +0900

    Fix testdata.sql

--- testdata/testdata.sql
+++ testdata/testdata.sql
@@ -34,8 +34,8 @@ VALUES (
   'Kindle'
 );
 
-INSERT INTO book_author (book_id, author_id) VALUES
-('d85f0e56-f632-4d50-b057-bab5af3d0159', '9d548ede-f636-4890-8418-ad3b7336d8e3'),
-('d85f0e56-f632-4d50-b057-bab5af3d0159', '54bbfcd2-e937-4da5-84fc-5984fa7b5979');
+INSERT INTO book_author (user_id, book_id, author_id) VALUES
+('testuser1', 'd85f0e56-f632-4d50-b057-bab5af3d0159', '9d548ede-f636-4890-8418-ad3b7336d8e3'),
+('testuser1', 'd85f0e56-f632-4d50-b057-bab5af3d0159', '54bbfcd2-e937-4da5-84fc-5984fa7b5979');
 
 COMMIT;
