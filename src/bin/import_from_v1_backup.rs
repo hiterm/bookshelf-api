5cd5a6f6ed6ef47cecd2d697e41f1c2bc8c5c6f8
hiterm <hiterm23@gmail.com>
2022-05-22 16:09:43 +0900

    wip: Create entity

--- src/bin/import_from_v1_backup.rs
+++ src/bin/import_from_v1_backup.rs
@@ -1,6 +1,17 @@
 use std::{collections::HashMap, env, fs::File, io::Read, path::Path};
 
+use bookshelf_api::domain::{
+    entity::{
+        author::AuthorId,
+        book::{
+            Book, BookFormat, BookId, BookStore, BookTitle, Isbn, OwnedFlag, Priority, ReadFlag,
+        },
+    },
+    error::DomainError,
+};
 use serde::{Deserialize, Serialize};
+use time::OffsetDateTime;
+use uuid::Uuid;
 
 fn main() -> anyhow::Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -25,16 +36,50 @@ fn main() -> anyhow::Result<()> {
     file.read_to_string(&mut json)?;
     let backup: BookshelfBackup = serde_json::from_str(&json)?;
 
+    for (id, book) in backup.books {
+        let uuid = Uuid::new_v4();
+
+        // TODO: author
+
+        let author_ids = find_or_create_authors(book.authors);
+        let created_at = book.created_at.map_or_else(
+            || OffsetDateTime::now_utc(),
+            |time| OffsetDateTime::from_unix_timestamp(time.seconds),
+        );
+        let updated_at = book.updated_at.map_or_else(
+            || OffsetDateTime::now_utc(),
+            |time| OffsetDateTime::from_unix_timestamp(time.seconds),
+        );
+
+        let book = Book::new(
+            BookId::new(uuid)?,
+            BookTitle::new(book.title)?,
+            author_ids,
+            Isbn::new(book.isbn.unwrap_or_else(|| "".to_owned()))?,
+            ReadFlag::new(book.read.unwrap_or(false)),
+            OwnedFlag::new(book.owned.unwrap_or(false)),
+            Priority::new(book.priority.unwrap_or(50))?,
+            BookFormat::try_from(book.format.unwrap_or_else(|| "".to_owned()).as_str())?,
+            BookStore::try_from(book.store.unwrap_or_else(|| "".to_owned()).as_str())?,
+            created_at,
+            updated_at,
+        )?;
+    }
+
     Ok(())
+}
+
+fn find_or_create_authors(authors: Vec<String>) -> Vec<AuthorId> {
+    todo!()
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct BookshelfBackup {
-    books: HashMap<String, Book>,
+    books: HashMap<String, BackupBook>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-struct Book {
+struct BackupBook {
     title: String,
     authors: Vec<String>,
     isbn: Option<String>,
@@ -50,7 +95,7 @@ struct Book {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct Time {
     #[serde(rename(serialize = "_seconds"))]
-    seconds: u64,
+    seconds: i64,
     #[serde(rename(serialize = "_nanoseconds"))]
-    nanoseconds: u64,
+    nanoseconds: i64,
 }
