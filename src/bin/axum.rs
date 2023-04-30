3033e74ce697a1e7d49001ff980ff7d9172c3026
hiterm <hiterm23@gmail.com>
2023-05-01 00:15:25 +0900

    fix: Runtime errors

--- src/bin/axum.rs
+++ src/bin/axum.rs
@@ -2,14 +2,19 @@ use std::sync::Arc;
 
 use axum::{
     routing::{get, post},
-    Router,
+    Extension, Router,
 };
 use bookshelf_api::{
     dependency_injection::{dependency_injection, MI, QI},
     extractors::{self, claims::AppState},
     presentation::handler::graphql::{graphql_handler, graphql_playground_handler},
 };
+use http::{
+    header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
+    HeaderValue, Method,
+};
 use sqlx::postgres::PgPoolOptions;
+use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
 async fn main() {
@@ -34,20 +39,30 @@ async fn main() {
 
     let auth0_config = extractors::Auth0Config::default();
 
-    let allowed_origins = fetch_allowed_origins();
-
     let auth0_config = extractors::Auth0Config::default();
     let state = Arc::new(AppState { auth0_config });
+
+    let allowed_origins: Vec<_> = fetch_allowed_origins()
+        .into_iter()
+        .map(|origin| origin.parse::<HeaderValue>().unwrap())
+        .collect();
+    let cors_layer = CorsLayer::new()
+        .allow_origin(allowed_origins)
+        .allow_methods([Method::POST])
+        .allow_headers(vec![AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
 
     // build our application with a single route
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
         .route("/graphql", post(graphql_handler::<QI, MI>))
         .route("/graphql/playground", get(graphql_playground_handler))
-        .with_state(state);
+        .with_state(state)
+        .layer(Extension(query_use_case))
+        .layer(Extension(schema))
+        .layer(cors_layer);
 
     // run it with hyper on localhost:3000
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
