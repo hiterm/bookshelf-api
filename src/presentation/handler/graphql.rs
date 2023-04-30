3915389045d6bbc4070db537e2c095db20592d90
hiterm <hiterm23@gmail.com>
2023-04-30 17:30:05 +0900

    refactor: Rewrite handler with axum

--- src/presentation/handler/graphql.rs
+++ src/presentation/handler/graphql.rs
@@ -1,10 +1,13 @@
-use actix_web::{get, web, HttpResponse, Responder};
 use async_graphql::{
     dataloader::DataLoader,
     http::{playground_source, GraphQLPlaygroundConfig},
     EmptySubscription, Schema,
 };
-use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
+use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
+use axum::{
+    response::{Html, IntoResponse},
+    Extension,
+};
 
 use crate::{
     extractors::Claims,
@@ -12,32 +15,28 @@ use crate::{
     use_case::traits::{mutation::MutationUseCase, query::QueryUseCase},
 };
 
-pub async fn graphql<QUC, MUC>(
-    schema: web::Data<Schema<Query<QUC>, Mutation<MUC>, EmptySubscription>>,
-    query_use_case: web::Data<QUC>,
-    request: GraphQLRequest,
+async fn graphql_handler<QUC, MUC>(
+    schema: Extension<Schema<Query<QUC>, Mutation<MUC>, EmptySubscription>>,
+    Extension(query_use_case): Extension<QUC>,
     claims: Claims,
+    req: GraphQLRequest,
 ) -> GraphQLResponse
 where
     QUC: QueryUseCase + Clone,
     MUC: MutationUseCase,
 {
-    let query_use_case: QUC = query_use_case.as_ref().clone();
+    let query_use_case: QUC = query_use_case.clone();
     let author_loader = DataLoader::new(
         AuthorLoader::new(claims.clone(), query_use_case),
         actix_web::rt::spawn,
     );
 
     schema
-        .execute(request.into_inner().data(claims).data(author_loader))
+        .execute(req.into_inner().data(claims).data(author_loader))
         .await
         .into()
 }
 
-#[get("/graphql/playground")]
-pub async fn graphql_playground() -> impl Responder {
-    let source = playground_source(GraphQLPlaygroundConfig::new("/graphql"));
-    HttpResponse::Ok()
-        .content_type("text/html; charset=utf-8")
-        .body(source)
+async fn graphql_playground() -> impl IntoResponse {
+    Html(playground_source(GraphQLPlaygroundConfig::new("/graphql")))
 }
